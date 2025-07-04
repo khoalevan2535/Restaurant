051d4e6854f2aec531e0c154894c2dd4883fa340
ChanhSweet <nguyenthanhquyen2005@gmail.com>
2025-07-04 09:33:03 +0700

    Cập nhật DishJPA.java

--- GoldenBamboo/src/main/java/com/poly/goldenbamboo/repositories/DishJPA.java
+++ GoldenBamboo/src/main/java/com/poly/goldenbamboo/repositories/DishJPA.java
@@ -9,26 +9,12 @@ import org.springframework.data.repository.query.Param;
 import com.poly.goldenbamboo.entities.DishEntity;
 
 public interface DishJPA extends JpaRepository<DishEntity, Integer>{
-//	 @Query("SELECT md.dishe FROM Menu_DishEntity md " +
-//	           "JOIN md.menus m " +
-//	           "WHERE m.branch.id = :branchId AND m.isDefault = true")
-//	    List<DishEntity> findDefaultMenuDishesByBranch(@Param("branchId") Integer branchId);
-	 
-//	 @Query("SELECT md.dish FROM MenuDishEntity md " +
-//		       "JOIN md.menus m " +
-//		       "WHERE m.branch.id = :branch AND m.isDefault = true " +
-//		       "AND md.dish.category.id = :category")
-//		List<DishEntity> findDefaultMenuDishesByBranchAndCategory(@Param("branchId") Integer branchId,
-//		                                                          @Param("categoryId") Integer categoryId);
+
 
 @Query("SELECT d FROM DishEntity d WHERE d.name = ?1")
     DishEntity findByName(String name);
 
-    // @Query("SELECT d FROM DishEntity d JOIN Discount_DishEntity dd ON d.id =
-    // dd.dishe.id")
-    // List<DishEntity> findDiscountedDishes();
-
-    @Query("SELECT d FROM DishEntity d LEFT JOIN Discount_DishEntity dd ON d.id = dd.dishe.id WHERE dd IS NOT NULL")
-    List<DishEntity> findDiscountedDishes();
+@Query("SELECT d FROM DishEntity d JOIN d.discountDishes dd")
+List<DishEntity> findDiscountedDishes();
 
 }
